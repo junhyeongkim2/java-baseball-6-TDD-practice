38f8bef34bbc97494f3bc5ba1d9217bd355cc1fa
jun <ggprgrkjh@naver.com>
2023-10-26 01:11:08 +0900

    feat: 컴퓨터넘버생성테스트 및 메소드

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class BaseballGame {
 
     List<Integer> userNumber;
+    List<Integer> computerNumber;
     public void startGame() {
     }
-
 
     public List<Integer> changeUserNumberToList(String userNumber) {
         this.userNumber = new ArrayList<Integer>();
@@ -19,6 +19,12 @@ public class BaseballGame {
     }
 
 
+    public List<Integer> createComputerNumber() {
+        Computer computer = new Computer();
+        this.computerNumber = new ArrayList<>();
+        this.computerNumber = computer.generateRandomComputerNumber();
+        return this.computerNumber;
+    }
 }
 
 
--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import baseball.model.BaseballGame;
 import baseball.model.Computer;
+import baseball.model.User;
 import baseball.view.InputView;
 import java.io.ByteArrayInputStream;
 import java.util.List;
@@ -29,6 +30,36 @@ public class BaseballGameTest {
 
         Assertions.assertEquals(userNumber.size(),input.length());
     }
+
+
+    @Test
+    void 컴퓨터넘버생성테스트(){
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer>computerNumber = baseballGame.createComputerNumber();
+
+        Assertions.assertEquals(3,computerNumber.size());
+        for(int number : computerNumber){
+            Assertions.assertTrue(number>=1&&number<=9);
+        }
+
+    }
+
+//    @Test
+//    void 공상태판별테스트(){
+//
+//        BaseballGame baseballGame = new BaseballGame();
+//        Computer computer = new Computer();
+//        InputView inputView = new InputView();
+//
+//        List <Integer> computerNumber = computer.generateRandomComputerNumber();
+//        String input = inputView.readUserNumber();
+//        List <Integer> userNumber = baseballGame.changeUserNumberToList(input);
+//
+//
+//        baseballGame.compareTwoNumbers();
+//
+//
+//    }
 
 
 
