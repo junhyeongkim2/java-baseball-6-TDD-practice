b734443aa35d325aa5dab8f765293ec21eb3bc74
jun <ggprgrkjh@naver.com>
2023-10-25 22:51:26 +0900

    feat: BaseballGameControllerTest 생성

---
+++ src/test/java/baseball/BaseballGameControllerTest.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import baseball.controller.BaseballGameController;
+import org.junit.jupiter.api.Test;
+
+public class BaseballGameControllerTest {
+
+
+    @Test
+    void 게임시작(){
+        BaseballGameController baseballGameController = new BaseballGameController();
+    }
+
+
+
+
+
+
+}
