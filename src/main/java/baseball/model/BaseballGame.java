aaf584d7e357717614711aeb2e01cf7a2f51967a
jun <ggprgrkjh@naver.com>
2023-10-26 01:03:23 +0900

    feat: 유저입력리스트 변환 테스트

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -1,10 +1,25 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
+
+    List<Integer> userNumber;
     public void startGame() {
     }
 
 
+    public List<Integer> changeUserNumberToList(String userNumber) {
+        this.userNumber = new ArrayList<Integer>();
+        for(int i = 0 ; i < userNumber.length(); i++){
+            this.userNumber.add(userNumber.charAt(i)-'0');
+        }
+        return this.userNumber;
+    }
 
 
 }
+
+
+
--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -1,6 +1,13 @@
 package baseball;
 
 import baseball.model.BaseballGame;
+import baseball.model.Computer;
+import baseball.view.InputView;
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class BaseballGameTest {
 
@@ -8,10 +15,27 @@ public class BaseballGameTest {
     void 게임시작테스트(){
         BaseballGame baseballGame = new BaseballGame();
         baseballGame.startGame();
-
-
-
     }
+
+    @Test
+    void 유저입력리스트변환테스트(){
+        ByteArrayInputStream fakeInput = new ByteArrayInputStream("123".getBytes());
+        System.setIn(fakeInput);
+        BaseballGame baseballGame = new BaseballGame();
+        InputView inputView = new InputView();
+
+        String input = inputView.readUserNumber();
+        List<Integer> userNumber = baseballGame.changeUserNumberToList(input);
+
+        Assertions.assertEquals(userNumber.size(),input.length());
+    }
+
+
+
+
+
+
+
 
 
 }
